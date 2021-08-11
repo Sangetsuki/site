a59cae8f1060fafdacd5cbafd056e97b60150ce6
Luiz <lzeduardo64@gmail.com>
2021-08-11 14:23:14 -0300

    add any to req and res types (ts-node does not reconize Request and Response types)

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 import path from 'path';
 import router from "./src/router";
 
@@ -9,7 +8,7 @@ app.use(express.json());
 app.use(express.static("./client/build"));
 const PORT = 8000;
 
-app.get('*', (req, res) => {
+app.get('*', (req: any, res: any) => {
 	res.sendFile(path.resolve("./client/build/index.html"));
 });
 
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,16 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 
-const router = express.Router();
+const router = Router();
+router.use(express.json());
 
 let internalCount = 0;
 let pkmn: any[] = [];
 
-router.get('/', (req, res) => {
+router.get('/', (req: any, res: any) => {
 	res.json({ pokemons: pkmn, count: internalCount });
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req: any, res: any) => {
 	console.log(`recebi: `);
 	console.log(req.body);
 	internalCount = pkmn.push(req.body);
